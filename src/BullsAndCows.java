7091523c453812931ff7f852f57cbbb4b7282671
jax-01 <jrcastillo6@up.edu.ph>
2024-08-24 21:10:30 +0800

    Check which digits in user guess matches code digits and increment bulls and cows variable accordingly

--- src/BullsAndCows.java
+++ src/BullsAndCows.java
@@ -2,7 +2,19 @@ package src;
 
 public class BullsAndCows {
     public static void main(String[] args) {
-        // Generates 4-digit code
+        // Generate a 4-digit code
         final short CODE = (short) (Math.random() * (9999 - 1000 + 1) + 1_000);
+        final String strCode = Short.toString(CODE);
+        String guess = new java.util.Scanner(System.in).nextLine();
+        int bulls = 0, cows = 0;
+
+        for (int i = 0; i < strCode.length(); i++) {
+            if (guess.charAt(i) == strCode.charAt(i)) {
+                bulls++;
+            } else if (guess.charAt(i) != strCode.charAt(i)
+                        && strCode.contains(String.valueOf(guess.charAt(i)))) {
+                cows++;
+            }
+        }
     }
 }
