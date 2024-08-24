ac30feba11e9fa118ba7c4b027c6f52d9e99d697
jax-01 <jrcastillo6@up.edu.ph>
2024-08-24 20:41:35 +0800

    Generate 4-digit code

--- src/BullsAndCows.java
+++ src/BullsAndCows.java
@@ -1,4 +1,8 @@
 package src;
 
 public class BullsAndCows {
+    public static void main(String[] args) {
+        // Generates 4-digit code
+        final short CODE = (short) (Math.random() * (9999 - 1000 + 1) + 1_000);
+    }
 }
