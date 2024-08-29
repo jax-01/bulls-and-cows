b94c7e824041202b862345ee41fbfc18ffa40075
jax-01 <jrcastillo6@up.edu.ph>
2024-08-29 17:50:31 +0800

    Terminate if user input for possible symbol is greater than 36

--- src/BullsAndCows.java
+++ src/BullsAndCows.java
@@ -18,6 +18,11 @@ public class BullsAndCows {
         System.out.println("Input the number of possible symbols in the code:");
         int possibleSymbols = sc.nextInt();
 
+        if (possibleSymbols > 36) {
+            System.out.println("Error: " + possibleSymbols + " exceeds the number of possible symbols in the code");
+            return;
+        }
+
         if (codeLength > possibleSymbols) {
             System.out.println("Code length shouldn't be greater than number of symbols");
             return;
